d5fbd2cdb08fe2601d168d66d1a4e7f8b0387551
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-12 01:32:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   # Need to return first name of that season winner
-  puts data[season]
+  puts data
+  puts season
 end
 
 def get_contestant_name(data, occupation)
