7e8cc3e53dd8f0ea1c4eccdcc775e48b92e8a2ef
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-12 01:40:30 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,9 @@
 def get_first_name_of_season_winner(data, season)
   # Need to return first name of that season winner
   data[season].each do |contestants|
+    #puts contestants
     if contestants["status"].downcase == "winner"
+      puts contestants
       return contestants["name"].split(" ").first
     end
   end
