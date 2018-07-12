5f764aab8d5402c033a5bb62602da9695c0af554
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-12 02:54:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,7 +46,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestants_data|
     sum += contestants_data["age"].to_i
   end
-  return (sum / divisor).round(0)
+  return (sum / divisor.to_f).round(0)
 end
 
 
